40a1f9fe576f35de0e0e76ef57da84942ea55cc5
Prasanna <Deva.Prasanna@Staples.com>
2020-07-02 19:59:30 -0400

    answer to xor and all function

--- all.js
+++ all.js
@@ -1,6 +1,21 @@
 function all(arr) {
 	// Loop through every item in the array
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] == false) {
+			return false
+		}
+	}
+	return true
+
+}
 	// If the item evaluates to false, return false
 
+	let array1 = [true, false, false]
+	let array2 = [true, true, true]
+
+	console.log(all(array1))
+	console.log(all(array2))
+
 	// If the loop ends, return true
-}
+
+
--- xor.js
+++ xor.js
@@ -1,7 +1,17 @@
 function xor(cond1, cond2) {
 	// if cond1 is true and cond2 is false return true
-
 	// if cond1 is false and cond2 is true return true
-
 	// return false
+	if (cond1 === true && cond2 === false) {
+		return true
+	}
+	//else {
+	//	return false
+	//}
 }
+console.log(xor(true, false))
+console.log(xor(true, true))
+console.log(xor(false, true))
+console.log(xor(false, false))
+
+
